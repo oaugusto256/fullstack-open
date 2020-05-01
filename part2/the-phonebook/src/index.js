4a762dbb256e1edaec73454374e73c5c6503859c
Otavio Augusto <tavioalves@gmail.com>
2020-05-01 16:54:09 -0300

    feat: Finish 2.6 the-phonebook step1

--- part2/the-phonebook/src/index.js
+++ part2/the-phonebook/src/index.js
@@ -7,21 +7,39 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('');
+
+  const handleAddNewPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName
+    };
+
+    setPersons([
+      ...persons,
+      personObject
+    ]);
+    setNewName('');
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={handleAddNewPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
