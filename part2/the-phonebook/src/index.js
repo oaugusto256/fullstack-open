cdc399d67d05f409c1423dd556ee63dbf3af43b4
Otavio Augusto <tavioalves@gmail.com>
2020-05-01 16:48:18 -0300

    chore and feat: Create the-phonebook project and add init boilerplate

---
+++ part2/the-phonebook/src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
