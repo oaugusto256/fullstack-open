c0361b9594370430b82600946cce9409d1d5e137
Otavio Augusto <tavioalves@gmail.com>
2020-05-05 09:46:23 -0300

    feat: Refactor notes to handle post and update http requests, create a services file, separate it concerns and refactor general code

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import ReactDOM from 'react-dom';
+
+import noteService from './services/notes';
 
 import './index.css';
 
@@ -12,11 +13,11 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data);
-      });
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
   }, []);
 
   const addNote = (event) => {
@@ -29,8 +30,12 @@ const App = () => {
       id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   };
 
   const handleNoteChanges = (event) => setNewNote(event.target.value);
@@ -41,6 +46,20 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important === true);
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -49,7 +68,7 @@ const App = () => {
       </button>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
