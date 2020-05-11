89d919996ce5d32a06cb30b192552c0c6e6e69a2
Otavio Augusto <tavioalves@gmail.com>
2020-05-11 16:38:35 -0300

    feat: Finish exercise 3.12 command-line database to add new phonebook persons and notes as well

---
+++ part3/backend/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack-oaugusto:${password}@cluster0-gsg4v.mongodb.net/notes-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(response => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
