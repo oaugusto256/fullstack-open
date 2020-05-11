608d27a66579eebd27a8d039fca72c3d42b25b57
Otavio Augusto <tavioalves@gmail.com>
2020-05-10 22:07:48 -0300

    feat: Add new config to handle production with notes app front-end and back-end

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+import axios from 'axios';
+const baseUrl = '/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl)
