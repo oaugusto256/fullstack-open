a2b9375341a8ea8357158bc274ae1e94878a7c73
Otavio Augusto <tavioalves@gmail.com>
2020-05-12 15:16:08 -0300

    feat: Add feat to notes save, find and delete notes from db. Add new errors handlers as well as put them at the correct code local

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -10,6 +10,18 @@ app.use(express.static('build'));
 
 const Note = require('./models/note');
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 app.get('/', (req, res) => {
   res.send('<h1>Notes API</h1>')
 })
@@ -38,18 +50,47 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note.toJSON())
-  });
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note.toJSON());
+      } else {
+        res.status(404).end();
+      };
+    })
+    .catch(error => next(error));
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON());
+    })
+    .catch(error => next(error));
 })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   notes = notes.filter(note => note.id !== id)
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-//   response.status(204).end()
-// });
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- part3/notes-backend/models/note.js
+++ part3/notes-backend/models/note.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useFindAndModify', false);
+
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
