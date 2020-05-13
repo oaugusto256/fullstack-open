9ccf1084151b0a0399ec39a6c4dd4bb92ad5a3bc
Otavio Augusto <tavioalves@gmail.com>
2020-05-12 22:38:28 -0300

    feat: Phonebook backend can handle delete person entity from specific id

--- part3/the-phonebook-backend/index.js
+++ part3/the-phonebook-backend/index.js
@@ -60,10 +60,11 @@ app.post('/api/persons', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.get('/info', (req, res) => {
