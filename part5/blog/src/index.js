dabce84fb8d9fc8c0d37d3d63d90aab7fd43a7dc
Otavio Augusto <tavioalves@gmail.com>
2020-05-20 22:25:06 -0300

    feat: Add styles to input and button tag to be able to inspect on chrome inspect mode, add handler to form submit, create services folder with login file to handle user login

--- part5/blog/src/index.js
+++ part5/blog/src/index.js
@@ -1,14 +1,57 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
+import loginService from "./services/login";
+
+import "./index.css";
 
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username, password,
+      });
+
+      console.log("Login was made successfully!");
+
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("Wrong credentials");
+    }
+  };
 
   return (
-    <h1>Blog</h1>
+    <>
+      <h1>Blog</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            name="Username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            name="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
   );
 };
 
@@ -16,5 +59,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
