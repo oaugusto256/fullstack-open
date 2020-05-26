ab0ca662791a791007cbca6b59dbc79d6f9239e8
Otavio Augusto <tavioalves@gmail.com>
2020-05-26 18:57:53 -0300

    feat: Add toggle function to show details of post component

--- part5/blog/src/components/postDetail.js
+++ part5/blog/src/components/postDetail.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PostDetail = ({ post }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <div className="post">
-      <h2>{post.title}</h2>
-      <p>{post.author}</p>
-      <p>{post.likes}</p>
+      <div className="post-row">
+        <h2>{post.title}</h2>
+        <button onClick={() => setShowDetails(!showDetails)}>{`${!showDetails ? 'Show details' : 'Close details'}`}</button>
+      </div>
+      {showDetails && (
+        <>
+          <p>{post.author}</p>
+          <p>{post.likes}</p>
+        </>
+      )}
     </div>
   );
 };
