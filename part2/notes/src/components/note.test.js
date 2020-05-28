0762a62d31177ed24afb89b1fcfc1a50a792d54d
Otavio Augusto <tavioalves@gmail.com>
2020-05-28 20:10:56 -0300

    test: Add note.test and togglable.test and start to play around it, following the part5 testing react apps

--- part2/notes/src/components/note.test.js
+++ part2/notes/src/components/note.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 
 import { prettyDOM } from "@testing-library/dom";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import Note from "./note";
 
@@ -37,4 +37,22 @@ test("renders content", () => {
   expect(div).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
+});
+
+test("clicking the button calls event handler once", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true
+  };
+
+  const mockHandler = jest.fn();
+
+  const component = render(
+    <Note note={note} toggleImportance={mockHandler} />
+  );
+
+  const button = component.getByText("make not important");
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
--- part2/notes/src/components/togglable.js
+++ part2/notes/src/components/togglable.js
@@ -16,7 +16,7 @@ const Togglable = (props) => {
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={showWhenVisible}>
+      <div style={showWhenVisible} className="togglableContent">
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
