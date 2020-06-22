d04e9858b829de31125cbb1b3793dc0308750e11
Otavio Augusto <tavioalves@gmail.com>
2020-06-22 20:43:36 -0300

    feat: Create part6 folder, add new project redux-notes and start to install all dependencies related to this project

---
+++ part6/redux-notes/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createStore } from 'redux'
+
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    return state.concat(action.data)
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map(note =>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+
+store.subscribe(renderApp)
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+})
