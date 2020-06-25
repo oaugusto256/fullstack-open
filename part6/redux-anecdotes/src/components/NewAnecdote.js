6d2dcd3b0faeb270bc5e131cac905ea04120f403
Otavio Augusto <tavioalves@gmail.com>
2020-06-24 21:46:49 -0300

    feat: feature to handle new anecdotes being add to the db

--- part6/redux-anecdotes/src/components/NewAnecdote.js
+++ part6/redux-anecdotes/src/components/NewAnecdote.js
@@ -5,16 +5,19 @@ import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification, resetState } from '../reducers/notificationReducer'
 
+import anecdotesService from "../services/anecdotes";
+
 const NewAnecdote = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
 
     const content = event.target.anecdote.value
 
     event.target.anecdote.value = ''
 
+    await anecdotesService.createNew(content);
     dispatch(createAnecdote(content))
     dispatch(showNotification('New anecdote created successfuly!'));
 
