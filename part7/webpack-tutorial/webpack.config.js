9fbfec53e16f4cfc01731d74a2ba5cc43dfb7c9b
Otavio Augusto <tavioalves@gmail.com>
2020-07-09 21:56:30 -0300

    feat: Add all transpiling and loaders config, add some other webpack configs

--- part7/webpack-tutorial/webpack.config.js
+++ part7/webpack-tutorial/webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path')
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
+const config = (env, argv) => {
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
   }
 }
 
