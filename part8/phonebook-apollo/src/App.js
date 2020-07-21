f96fd56217999f0ce3af7e4f0e7cb3b16e0009f3
Otavio Augusto <tavioalves@gmail.com>
2020-07-20 21:42:04 -0300

    feat: Start to fetch allPersons from apollo-server phonebook on phonebook client react app

--- part8/phonebook-apollo/src/App.js
+++ part8/phonebook-apollo/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_PERSONS = gql`
+query {
+  allPersons  {
+    name
+    phone
+    id
+  }
+}
+`
 
 function App() {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {result.data.allPersons.map(p => p.name).join(', ')}
     </div>
-  );
+  )
 }
 
 export default App;
--- part8/phonebook-apollo/src/index.js
+++ part8/phonebook-apollo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -11,23 +11,9 @@ const client = new ApolloClient({
   })
 })
 
-const query = gql`
-query {
-  allPersons  {
-    name,
-    phone,
-    address {
-      street,
-      city
-    }
-    id
-  }
-}
-`
-
-client.query({ query })
-  .then((response) => {
-    console.log(response.data)
-  })
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ApolloProvider client={client} >
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
